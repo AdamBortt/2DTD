d6b93db11b4ed8bbd104e94388068807ae5884d5
AdamBortt <bortkun62@gmail.com>
2024-06-06 17:10:14 +0200

    Turrets can deal damage to enemeis

--- Scenes/Turrets/all_turrets.gd
+++ Scenes/Turrets/all_turrets.gd
@@ -3,13 +3,17 @@ extends Node2D
 var type
 var enemy_array = []
 var enemy
+var turret_ready = true
 
 func _ready():
 	self.get_node("Range/CollisionShape2D").get_shape().radius = 0.5 * GameData.tower_data[type]["range"]
+
 func _physics_process(delta):
 	if enemy_array.size() != 0:
 		select_enemy()
 		turn()
+		if turret_ready:
+			fire()
 	else:
 		enemy = null
 		
@@ -23,6 +27,12 @@ func select_enemy():
 	
 func turn():
 	get_node("Turret").look_at(enemy.position)
+	
+func fire():
+	turret_ready = false
+	enemy.on_hit(GameData.tower_data[type]["damage"])
+	await(get_tree().create_timer(GameData.tower_data[type]["rof"]).timeout)
+	turret_ready = true
 	
 func _on_range_body_entered(body):
 	enemy_array.append(body.get_parent())
