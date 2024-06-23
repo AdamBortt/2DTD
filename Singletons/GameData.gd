e1fe9e295319016555fd3eaabe0df26a3c12db86
AdamBortt <bortkun62@gmail.com>
2024-06-23 14:50:45 +0200

    Added enemy data to global variables

--- Singletons/GameData.gd
+++ Singletons/GameData.gd
@@ -4,9 +4,17 @@ var tower_data = {
 	"T1_basic": {
 		"damage": 20,
 		"rof": 1,
-		"range": 150},
+		"range": 150,
+		"category": "Projectile"},
 	"T1_sniper": {
 		"damage": 100,
 		"rof": 4,
-		"range": 250}
+		"range": 250,
+		"category": "Laser"}
+}
+
+var enemy_data = {
+	"normal_1": {
+		"speed": 50,
+		"hp": 50}
 }
