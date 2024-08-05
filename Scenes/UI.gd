a59f46a6868b109d78bf5955479ac53edc084b01
AdamBortt <bortkun62@gmail.com>
2024-08-05 20:02:46 +0200

    Fixed incorrect wave counting

--- Scenes/UI.gd
+++ Scenes/UI.gd
@@ -5,7 +5,6 @@ func _on_pause_play_pressed():
 	if get_tree().is_paused():
 		get_tree().paused = false
 	elif get_parent().break_state == true:
-		get_parent().current_wave += 1
 		get_parent().start_next_wave()
 		get_parent().break_state = false
 	else:
